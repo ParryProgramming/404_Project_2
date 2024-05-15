19f908abc7184b6e4a6239bb08ddee53cdaff765
Sweaty Hog <Royceatkins93@gmail.com>
2024-05-15 13:33:18 -0400

    routing for handlebars

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
+
 const apiRoutes = require('../controllers/api');
 const homeRoutes = require('./homeRoutes');
 
 router.use('/api', apiRoutes);
-
 router.use('/', homeRoutes);
 
 router.use((req, res) => {
