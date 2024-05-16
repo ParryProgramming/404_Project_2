61c2465063f0aa6d65cd8a12c783ec040e7b21f9
Christian Abbate <Christian@myabbate.com>
2024-05-16 15:49:16 -0400

    adjusting styling

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -76,7 +76,7 @@ router.get('/profile', withAuth, async (req, res) => {
     const user = userData.get({ plain: true });
 
     res.render('profile', {
-      ...user,
+      user,
       logged_in: true
     });
   } catch (err) {
